d74923ad87f0b67707bee047420d976f89fbd0a1
tikotoi <101997239+tikotoi@users.noreply.github.com>
2022-04-21 10:33:35 +0400

    final final

--- script.js
+++ script.js
@@ -105,9 +105,9 @@
 // let array = [32, 10, 'hello', null, 'hello2', 50 ]
 
 // for ( let x of array) {
-//         if (x % 5 == 0) 
+//         if (typeof x=='number' && x % 5 == 0) 
 //         console.log(x);
-//     }
+//     }s
 
 
 //  function 
